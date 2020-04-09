37be8f441fc642af0589cbdd86024bf5f18a2c63
Ruben Lopez <vrchavez07@gmail.com>
2020-04-08 18:43:06 -0700

    changed api calls

--- src/app/services/chefserv.service.ts
+++ src/app/services/chefserv.service.ts
@@ -7,8 +7,8 @@ import { catchError } from 'rxjs/operators';
 })
 
 export class ChefservService {
-  baseUrl: string = 'http://localhost:50025/api';
-  //baseUrl: string = 'https://lockdown20200408122040.azurewebsites.net';
+  //baseUrl: string = 'http://localhost:50025/api';
+  baseUrl: string = 'https://lockdown20200408122040.azurewebsites.net';
   constructor(private http: HttpClient) { }
 
   addFood(food) {
